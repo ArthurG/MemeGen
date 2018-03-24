dd75b75af55f04eeab5339fc9759424165f5faeb
Arthur Guo <arthur.guo@uwaterloo.ca>
2018-03-24 21:31:08 +0000

    Add script to download to s3

---
+++ download_img.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import time
+import requests
+
+import boto3
+
+s3 = boto3.resource('s3')
+
+def download(image_url, title):
+    img_data = requests.get(image_url).content
+    with open("photos/"+title, 'wb') as handler:
+        handler.write(img_data)
+
+df = pd.read_csv("dec17scrape.csv")
+imgs = []
+for url, score in zip(df["url"], df["score"]):
+    t = time.time()
+    if url.endswith(".jpg"):
+        img = "{}_{}.jpg".format(t, score)
+    elif url.endswith(".png"):
+        img = "{}_{}.jpg".format(t, score)
+    download(url, img)
+
+    data = open('photos/'+img, 'rb')
+    s3.Bucket('arthur-memes').put_object(Key=img, Body=data)
+    imgs.append(img)
+
